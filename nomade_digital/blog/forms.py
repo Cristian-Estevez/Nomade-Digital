48fdefce584657b8eee542c06669ebe612890e6c
David <fernandezdavid1992@gmail.com>
2021-01-26 21:02:41 -0300

    ultimos cambios css antes de python anywhere

--- nomade_digital/blog/forms.py
+++ nomade_digital/blog/forms.py
@@ -11,6 +11,8 @@ class PostForm(forms.ModelForm):
 
 class PostComentForm(forms.ModelForm):
     texto = forms.CharField(widget=forms.Textarea(attrs={'class': 'text-area-comentario'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'type': 'email', 'placeholder': '¿Cual es tu E-Mail'}))
+    nombre_usuario = forms.CharField(widget=forms.TextInput(attrs={'placeholder': '¿Como te llamas?'}))
     class Meta:
         model = PostComent
         fields = ['nombre_usuario', 'email', 'texto']
--- nomade_digital/index/forms.py
+++ nomade_digital/index/forms.py
@@ -4,9 +4,9 @@ from .models import Consulta
 
 class ConsultaForm(forms.ModelForm):
 
-    nombre = forms.CharField(widget=forms.TextInput(attrs={'placeholder': '¿Como te llamas?', 'class': 'con'}))
-    email = forms.EmailField(widget=forms.TextInput(attrs={'type': 'email', 'placeholder': '¿Cual es tu E-Mail'}))
-    mensaje = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Enviame tu consulta...'}))
+    nombre = forms.CharField(widget=forms.TextInput(attrs={'placeholder': '¿Como te llamas?', 'class': 'inputs-datos'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'type': 'email', 'placeholder': '¿Cual es tu E-Mail', 'class': 'inputs-datos'}))
+    mensaje = forms.CharField(widget=forms.Textarea(attrs={'class': 'text-area-comentario', 'placeholder': 'Enviame tu consulta...'}))
 
     # FALTA GENERAR UN CLASS EN CSS Y AGREGARSELO A DICC DE ATTRS EN CADA CAMPO
     class Meta:
